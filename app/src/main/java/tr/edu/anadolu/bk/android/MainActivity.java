ef51b628d0eae5caaadb9d5e98ad140377b744b6
Oguzhan Uzman <ouzmann@gmail.com>
2016-08-01 03:32:19 +0300

    TabLayout integration completed via 3 tabs.

--- app/src/main/java/tr/edu/anadolu/bk/android/MainActivity.java
+++ app/src/main/java/tr/edu/anadolu/bk/android/MainActivity.java
@@ -1,12 +1,27 @@
 package tr.edu.anadolu.bk.android;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.viewpager)
+    ViewPager viewPager;
+    @BindView(R.id.tabs)
+    TabLayout tabLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        viewPager.setAdapter(new TabFragmentPagerAdapter(getSupportFragmentManager(), MainActivity.this));
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
